182072f3780391a8c8dc13d27f29f5faef1b5b21
Emi ONeal <atashiemichandesu@gmail.com>
2025-01-18 02:42:21 -0500

    removed json import

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { json } from "express/lib/response";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
