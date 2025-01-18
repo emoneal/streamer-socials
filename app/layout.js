4423aaeed243c4bd1c5320d96ba2818f5c37ed3b
Emi ONeal <atashiemichandesu@gmail.com>
2025-01-18 04:34:42 -0500

    fixed og

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "PixelEmii - Socials",
-  description: "PixelEmii is a cyberpunk-themed VTuber with a mischievous, playful personality and a love for gaming. Join the chaos at the Byte & Brew Cafe!",
+  description: "PixelEmii is a cyberpunk-themed hacker bunny VTuber with a mischievous, playful personality and a love for gaming. Join the chaos at the Byte & Brew Cafe!",
   keywords: "PixelEmii, Pixel, Emii, Byte & Brew Cafe, Cafe VTuber, PixelCafe, PixelCafeVT, ENVTuber, VTuberEN, English VTuber, Twitch VTuber, YouTube VTuber, trans VTuber, bunny VTuber, bunnygirl, rabbit girl, cozy streamer, gaming streamer, MMORPG VTuber, simulation games, Final Fantasy XIV VTuber, Sims VTuber, Fortnite streamer, variety streamer, nostalgic gamer, interactive entertainment, humor, chill vibes, inclusive community, LGBTQ+ friendly streamer, trans representation, digital adventures, VR gaming, engaging streamer, charismatic personality, creativity and passion, virtual cafe experience, retro gaming, NES games, story-rich games, adventure games, red teamer streamer, cybersecurity gamer, ethical hacker gamer, TryHackMe VTuber, gamer girl vibes, indie game enthusiast, streamer community building, parasocial interaction, cozy digital spaces, online friendships, wholesome content, Pixel Café vibes, bunny girl aesthetic, pastel gaming world, casual gaming fun, strategic simulation games, escapism, comfy gaming sessions, Twitch streamer, YouTuber, cyberpunk, cyberpunk vtuber, hacker vtuber, cybersecurity vtuber, tutorials",
   name: "viewport",
   content: "width=device-width, initial-scale=1",
@@ -23,7 +23,7 @@ export const metadata = {
     title: "PixelEmii - Socials",
     description: "PixelEmii is a cyberpunk-themed hacker bunny VTuber with a mischievous, playful personality and a love for gaming. Join the chaos at the Byte & Brew Cafe!",
     url: "https://socials.pixelemii.com",
-    image: "https://www.pixelemii.com/ogpixelcafe.png",
+    image: "https://socials.pixelemii.com/ogpixelcafe.png",
     site_name: "PixelEmii",
     type: "website",
   },
@@ -31,7 +31,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "PixelEmii - Socials",
     description: "PixelEmii is a cyberpunk-themed hacker bunny VTuber with a mischievous, playful personality and a love for gaming. Join the chaos at the Byte & Brew Cafe!",
-    image: "https://www.pixelemii.com/ogpixelcafe.png",
+    image: "https://socials.pixelemii.com/ogpixelcafe.png",
   }
 };
 
@@ -43,14 +43,14 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta property="og:title" content={metadata.openGraph.title} />
         <meta property="og:description" content={metadata.openGraph.description} />
-        <meta property="og:image" content="https://socials.pixelemii.com/ogpixelcafe.png" />
+        <meta property="og:image" content={metadata.openGraph.image} />
         <meta property="og:url" content={metadata.openGraph.url} />
         <meta property="og:site_name" content={metadata.openGraph.site_name} />
         <meta property="og:type" content={metadata.openGraph.type} />
         <meta name="twitter:card" content={metadata.twitter.card} />
         <meta name="twitter:title" content={metadata.twitter.title} />
         <meta name="twitter:description" content={metadata.twitter.description} />
-        <meta name="twitter:image" content="https://socials.pixelemii.com/ogpixelcafe.png" />
+        <meta name="twitter:image" content={metadata.twitter.image} />
       </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
